bb9bd3eca65c4806d714668d421c266525841489
Ope Adeyomoye <victoradeyomoye@yahoo.co.uk>
2025-01-29 23:15:49 +0100

    Remove unused screens

--- src/layout/user.vue
+++ src/layout/user.vue
@@ -49,9 +49,9 @@
                 </div>
                 <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
                   <MenuItems class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                    <MenuItem v-slot="{ active }">
+                    <!-- <MenuItem v-slot="{ active }">
                       <router-link to="/settings" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Settings</router-link>
-                    </MenuItem>
+                    </MenuItem> -->
                     <MenuItem v-slot="{ active }">
                       <a href="#" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']" @click.prevent="signOut">Sign out</a>
                     </MenuItem>
@@ -138,8 +138,6 @@ import { auth } from '@/auth/firebase'
 const navigation = [
   { name: 'dashboard', href: '/', },
   { name: 'send', href: '/send', },
-  { name: 'settings', href: '/settings' },
-  { name: 'tests', href: '/tests' },
 ]
 const userNavigation = [
   { name: 'Your Profile', href: '#' },
